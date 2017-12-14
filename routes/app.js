b4faa3124bfb92220d31ec44ac8aac35dc3ac8a8
recai <recai.gunay@gmail.com>
2017-12-15 00:13:41 +0300

    ready to go for router change

--- routes/app.js
+++ routes/app.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/js/app/bundle.js', function(req, res){
+    res.sendfile(__dirname + '/bundle.js');
+  });
+  
+  router.get('/stylesheets/style.css', function(req, res){
+    res.sendfile(__dirname + '/stylesheets/style.css');
+  });
+
+  
 router.get('/', function (req, res, next) {
     res.render('index');    
 });
