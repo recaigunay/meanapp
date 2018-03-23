eecfbcb0e0e647c8926088a5f6f3b0ef2229926b
recai <recai.gunay@gmail.com>
2018-03-24 01:27:23 +0300

    product detail is changed again

--- assets/app/products/product-detail.component.ts
+++ assets/app/products/product-detail.component.ts
@@ -1,43 +1,133 @@
-import {Component, Input, OnInit} from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { Product } from "./product.model";
 import { ProductService } from "./product.service";
 import { ActivatedRoute } from "@angular/router";
 
 @Component({
-selector : 'app-product-detail',
-templateUrl: './product-detail.component.html',
-styles: [`  
-  .ui.image {
-    width:300px;
-    height:250px;
-    background-size: cover;
-    max-height: 250px;
-    max-width: 300px;
-    min-height: 250px;
-    min-width: 300px;
+    selector: 'app-product-detail',
+    templateUrl: './product-detail.component.html',
+    styles: [`  
+    *{font-family: 'Roboto', sans-serif;}
+
+@keyframes click-wave {
+  0% {
+    height: 40px;
+    width: 40px;
+    opacity: 0.35;
+    position: relative;
   }
+  100% {
+    height: 200px;
+    width: 200px;
+    margin-left: -80px;
+    margin-top: -80px;
+    opacity: 0;
+  }
+}
+
+.option-input {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  -ms-appearance: none;
+  -o-appearance: none;
+  appearance: none;
+  position: relative;
+  top: 13.33333px;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  height: 40px;
+  width: 40px;
+  transition: all 0.15s ease-out 0s;
+  background: #ffffff;
+  border-radius: 10px;
+  border: 1px solid gray;
+  color: #fff;
+  cursor: pointer;
+  display: inline-block;
+  margin-right: 0.5rem;
+  outline: none;
+  position: relative;
+  z-index: 1000;
+}
+.option-input:hover {
+  background: #9faab7;
+}
+.option-input:checked {
+  background: #cbd1d8;
+}
+.option-input:checked::before {
+  height: 40px;
+  width: 40px;
+  position: absolute;
+  content: '✔';
+  display: inline-block;
+  font-size: 26.66667px;
+  text-align: center;
+  line-height: 40px;
+}
+.option-input:checked::after {
+  -webkit-animation: click-wave 0.65s;
+  -moz-animation: click-wave 0.65s;
+  animation: click-wave 0.65s;
+  background: #40e0d0;
+  content: '';
+  display: block;
+  position: relative;
+  z-index: 100;
+}
+.option-input.radio {
+  border-radius: 50%;
+}
+.option-input.radio::after {
+  border-radius: 50%;
+}
+
+body {
+  display: -webkit-box;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: box;
+  background: #e8ebee;
+  color: #9faab7;
+  font-family: "Helvetica Neue", "Helvetica", "Roboto", "Arial", sans-serif;
+  text-align: center;
+}
+body div {
+  padding: 5rem;
+}
+body label {
+  display: block;
+  line-height: 40px;
+}
+
+    
   
 `]
 })
 export class ProductDetailComponent implements OnInit {
-    id:string;
-    product : Product ;
-    products : Product[] ;
-    sub : any;
-    constructor(private productService:ProductService, private _route:ActivatedRoute) {
+    id: string;
+    product: Product;
+    products: Product[];
+    sub: any;
+    $ :any = window["$"];
+    constructor(private productService: ProductService, private _route: ActivatedRoute) {
 
     }
 
     ngOnInit() {
         this.sub = this._route.params.subscribe(params => {
-            this.id = params['id'].toString(); 
-                       this.productService.getProductDetail(this.id.toString()).subscribe(           
-                (products:Product[]) => {           
-                    if ( products.length>0) {
+            this.id = params['id'].toString();
+            this.productService.getProductDetail(this.id.toString()).subscribe(
+                (products: Product[]) => {
+                    if (products.length > 0) {
                         this.product = products[0];
                     }
                 }
-            );   
-         });
-    }  
+            );
+        });
+
+
+    }
+   
 }
